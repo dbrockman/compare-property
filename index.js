4f0f7100b06702751f5f3cf1d08747952273590a
David Brockman Smoliansky <david.bs@gmail.com>
2013-11-28 02:59:07 +0100

    compare checks hasOwnProperty
    compare function will treat a missing property as less than an existing property and null as less than any object

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ exports.ascending  = compare_ascending;
 exports.descending = compare_descending;
 exports.property   = compare_property;
 exports.properties = compare_properties;
+exports.compare    = compare;
 
 /**
 Sort in ascending order.
@@ -26,7 +27,7 @@ Ex. ['ab', 'a', 'abc'].sort(property('length')) // ['a', 'ab', 'abc']
 **/
 function compare_property(property, order) {
   return function (a, b) {
-    return _compare_property(a, b, property, -1 === order);
+    return compare(a, b, property, order);
   };
 }
 
@@ -36,33 +37,38 @@ Specify sort order as 1 (ascending) or -1 (descending), default is ascending.
 Ex. people.sort(properties({ age: -1, surname: 1 })) // Sort by oldest first and secondly on surname A to Z
 **/
 function compare_properties(properties) {
-  var keys   = Object.keys(properties)
-    , length = keys.length
-    , order  = []
-    , i      = 0;
+  var keys = Object.keys(properties),
+    length = keys.length,
+    order  = [],
+    i      = 0;
 
-  for (; i < length; i++)
-    order[i] = -1 === properties[keys[i]];
+  for (; i < length; i++) {
+    order[i] = properties[keys[i]];
+  }
 
   return function (a, b) {
-    var c = 0
-      , i = 0;
-
-    for (; !c && i < length; i++)
-      c = _compare_property(a, b, keys[i], order[i]);
-
+    var c = 0, i = 0;
+    for (; !c && i < length; i++) {
+      c = compare(a, b, keys[i], order[i]);
+    }
     return c;
   };
 }
 
-function _compare_property(a, b, property, descending) {
-  var c;
+function compare(a, b, property, order) {
+  var c, ap, bp;
   if (a && b) {
-    a = a[property];
-    b = b[property];
-    c = a > b ? 1 : a < b ? -1 : 0;
+    ap = a.hasOwnProperty(property);
+    bp = b.hasOwnProperty(property);
+    if (ap && bp) {
+      a = a[property];
+      b = b[property];
+      c = a > b ? 1 : a < b ? -1 : 0;
+    } else {
+      c = ap || bp ? ap ? 1 : -1 : 0;
+    }
   } else {
     c = a || b ? a ? 1 : -1 : 0;
   }
-  return descending ? -c : c;
+  return (-1 === order) ? -c : c;
 }
